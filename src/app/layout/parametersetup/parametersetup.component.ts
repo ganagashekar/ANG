dfbc04ba60daa04a0508561937ff6bb4775a776a
ganagashekar <ganga123081@gmail.com>
2019-10-17 23:19:11 +0530

    ParamSetupScreen

--- src/app/layout/parametersetup/parametersetup.component.ts
+++ src/app/layout/parametersetup/parametersetup.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray, ValidatorFn } from '@angular/forms';
+import { ParameterFilter } from 'src/app/Model/FilterModels/ParameterFilter';
+import { ReferenceRecords } from 'src/app/Model/ServiceResposeModel/CommonModel/ReferenceRecordsModel';
+import { MatTableDataSource, MatPaginator, MatSort, MatSnackBar, MatDialog } from '@angular/material';
+import { ParamModel } from 'src/app/Model/ServiceResposeModel/Setups/ParamModel';
+import { AppComponent } from 'src/app/app.component';
+import { ActivatedRoute } from '@angular/router';
+import { SetupsService } from 'src/app/shared/services/Setups.service';
 
 @Component({
   selector: 'app-parametersetup',
   templateUrl: './parametersetup.component.html',
   styleUrls: ['./parametersetup.component.scss']
 })
-export class ParametersetupComponent implements OnInit {
+export class ParametersetupComponent implements OnInit , AfterViewInit {
+  parameterFilter: ParameterFilter;
+  stacksArray: ReferenceRecords[] = [];
+  paramListDataSource: MatTableDataSource<ParamModel>;
+  displayedColumns: string[] = [
+    'editAction', 'confgid', 'paramname', 'paramunit',
+    'paramminval', 'parammaxval', 'threshholdval',
+    'paramposition', 'creatts', 'updtts',
+   'deleteAction'];
 
-  constructor() { }
+   receivedChildMessage: string;
+
+   @ViewChild(MatSort, { static: false }) sort: MatSort;
+   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+   constructor(private _dialog: MatDialog, private _appcomponent: AppComponent, private _route: ActivatedRoute,
+    private _setupsservices: SetupsService,
+    private snackBar: MatSnackBar) {
+    _appcomponent.currenturl = '/taskSchedulerList';
+    this.paramListDataSource = new MatTableDataSource<ParamModel>();
+    this.parameterFilter = new ParameterFilter();
+  }
 
   ngOnInit() {
+
+    this. getAllStacks();
+    this.getAllParameterList();
   }
+
+  ngAfterViewInit(): void {
+    this.paramListDataSource.sort = this.sort;
+    this.paramListDataSource.paginator = this.paginator;
+  }
+
+  StackChange(event) {
+    // this.schedulerFilter.IsActive = event.checked;
+    // this.getTaskSchedulerList();
+  }
+
+  getAllParameterList(): void {
+    this._setupsservices.getAllParameterList(this.parameterFilter).subscribe(resp => {
+     this.paramListDataSource.data = resp.model as ParamModel[];
+   }, error => {
+     console.log('Error: ' + error);
+   });
+ }
+getAllStacks(): void {
+  this._setupsservices.getAllStacks(0, true).subscribe(resp => {
+    this.stacksArray = resp.model as ReferenceRecords[];
+  }, error => {
+    console.log('Error: ' + error);
+  });
+}
+
 
 }
--- src/app/layout/parametersetup/parametersetup.module.ts
+++ src/app/layout/parametersetup/parametersetup.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParametersetupComponent } from './parametersetup.component';
+import {MaterialModule} from '../../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [ParametersetupComponent]
 })
-export class ParametersetupModule { }
+export class ParametersetupModule {
+
+
+}
